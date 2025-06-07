bed7e02ad6dfe8f6738bf70545a2c239a985b666
Lucas Atolini <atolini@live.com>
2025-06-06 22:16:08 -0300

    fix(database/repository/dynamo): add missing export and reorder existing ones
    Reordered the exports for clarity and maintainability.
    Also added an export that was previously missing.

--- src/services/database/repository/implementations/dynamo/index.ts
+++ src/services/database/repository/implementations/dynamo/index.ts
@@ -1,7 +1,10 @@
 export * from './dynamo-read-repository-error-handler';
 export * from './dynamo-read-repository-event-logger';
+export * from './dynamo-read-repository';
+
+export * from './dynamo-write-repository-error-handler';
 export * from './dynamo-write-repository-event-logger';
 export * from './dynamo-write-repository';
-export * from './dynamo-read-repository';
+
 export * from './dynamo-item';
 export * from './key';
