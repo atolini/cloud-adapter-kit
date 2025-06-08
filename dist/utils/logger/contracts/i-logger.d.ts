43822dcefe103a070a9c6a36bd2d3656183f01d3
Lucas Atolini <atolini@live.com>
2025-06-08 13:36:15 -0300

    build: update distribution files for version 4.1.1

--- dist/utils/logger/contracts/i-logger.d.ts
+++ dist/utils/logger/contracts/i-logger.d.ts
@@ -2,5 +2,6 @@ export interface ILogger<T> {
     warn(item: object | string): void;
     error(item: object | string): void;
     info(item: object | string): void;
+    updateContext(newContext: Partial<T>): void;
 }
 //# sourceMappingURL=i-logger.d.ts.map
