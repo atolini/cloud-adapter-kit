780aaa9dba969db8f10212ee65c5bc83be698702
Lucas Atolini <atolini@live.com>
2025-06-20 18:08:04 -0300

    refactor(dynamo-transaction-writer): remove unnecessary condition expression for item insertion

--- src/services/database/transactional-writer/implementations/dynamo/dynamo-transaction-writer.ts
+++ src/services/database/transactional-writer/implementations/dynamo/dynamo-transaction-writer.ts
@@ -163,19 +163,10 @@ export class DynamoTransactionWriter
         return result;
       }
 
-      const condition = hasSortKey
-        ? 'attribute_not_exists(#pk) AND attribute_not_exists(#sk)'
-        : 'attribute_not_exists(#pk)';
-
       const result = {
         Put: {
           TableName: container.getTableName(),
           Item: marshall({ ...item, version: newVersion }, { removeUndefinedValues: true }),
-          ConditionExpression: condition,
-          ExpressionAttributeNames: {
-            '#pk': pkName,
-            ...(hasSortKey && skName ? { '#sk': skName } : {}),
-          },
         },
       };
 
