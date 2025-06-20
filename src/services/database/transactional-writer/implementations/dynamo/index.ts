634d1ec4db514d9965e69e1388303297a3c58d7c
Lucas Atolini <atolini@live.com>
2025-06-20 16:10:30 -0300

    fix(database/schema/dynamo): add missing export for dynamo-transaction-writer-event-logger

--- src/services/database/transactional-writer/implementations/dynamo/index.ts
+++ src/services/database/transactional-writer/implementations/dynamo/index.ts
@@ -1,3 +1,4 @@
 export * from './dynamo-transaction-writer-error-handler';
 export * from './dynamo-transaction-writer';
 export * from './max-item-exceeded-error';
+export * from './dynamo-transaction-writer-event-logger';
